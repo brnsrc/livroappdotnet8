2afaab26058961c5e9d85b37874e73a4b74ed60e
Bruno Souza <brunosrc@hotmail.com>
2025-04-03 01:53:22 -0300

    Creating a console app for working with ADO.NET

---
+++ Chapter02/Northwind.Console.SqlClient/Program.Helpers.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Northwind.Console.SqlClient
+{
+    public class Program.Helpers
+    {
+        private static void ConfigureConsole(string culture="en-US", bool useComputerCulture = false)
+        {
+            //To enable Unicode character like euro symbol in the console
+        }
+    }
+}
