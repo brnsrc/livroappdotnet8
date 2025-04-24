63b1d8dd080f45a0e886f377745deedb89e1c4ef
Bruno Souza <brunosrc@hotmail.com>
2025-04-24 18:43:41 -0300

    Creating a test project to check the integration of the class libraries

---
+++ Chapter03/Northwind.Common.DataContext.SqlServer/NorthwindContextPartial.cs
@@ -0,0 +1,29 @@
+using Microsoft.Data.SqlClient; // SqlConnectionStringBuilder
+using Microsoft.EntityFrameworkCore; // DbContext
+
+namespace Northwind.EntityModels;
+public partial class NorthwindContext : DbContext
+{
+    private static readonly SetLastRefreshedInterceptor setLastRefreshedInterceptor = new();
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            SqlConnectionStringBuilder builder = new();
+            builder.DataSource = ".\\SQLEXPRESS";
+            builder.InitialCatalog = "Northwind";
+            builder.TrustServerCertificate = true;
+            builder.MultipleActiveResultSets = true;
+
+            //Because we want to fail. Default is 15 seconds.
+            builder.ConnectTimeout = 3;
+
+            //If using SQL Server authentication.
+            // builder.UserID = Environment.GetEnvironmentVariable("MY_SQL_USR");
+            // builder.Password = Environment.GetEnvironmentVariable("MY_SQL_PWD");
+            optionsBuilder.UseSqlServer(builder.ConnectionString);
+        }
+        optionsBuilder.AddInterceptors(setLastRefreshedInterceptor);
+    }
+}
+
---
+++ Chapter03/Northwind.Common.DataContext.SqlServer/SetLastRefreshedInterceptor.cs
@@ -0,0 +1,16 @@
+// IMaterializationInterceptor, MaterializationInterceptionData
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace Northwind.EntityModels;
+
+public class SetLastRefreshedInterceptor : IMaterializationInterceptor
+{
+    public object InitializedInstance(MaterializationInterceptionData materializationInterceptionData, object entity)
+    {
+        if (entity is IHasLastRefreshed entityWithLastRefreshed)
+        {
+            entityWithLastRefreshed.LastRefreshed = DateTimeOffset.UtcNow;
+        }
+        return entity;
+    }
+}
