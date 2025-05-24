fe83d253559284a3ac7fe74754d96b74ded5097c
Bruno Souza <brunosrc@hotmail.com>
2025-05-24 02:03:47 -0300

    Chapter 05 - Starting tasks

---
+++ Chapter05/WorkingWithTasks/Program.Methods.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+partial class Program
+{
+    private static void MethodA()
+    {
+        TaskTitle("Starting Method A ...");
+        OutputThreadInfo();
+        Thread.Sleep(3000); //Simulate three seconds of work.
+        TaskTitle("Finished Method A.");
+    }
+
+    private static void MethodB()
+    {
+        TaskTitle("Starting Method B ...");
+        OutputThreadInfo();
+        Thread.Sleep(2000); //Simulate two seconds of work.
+        TaskTitle("Finished Method B.");
+    }
+    private static void MethodC()
+    {
+        TaskTitle("Starting Method C ...");
+        OutputThreadInfo();
+        Thread.Sleep(1000); //Simulate one second of work
+        TaskTitle("Finished Method C.");
+    }
+}
---
+++ Chapter05/WorkingWithTasks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics; //To use Stopwatch
+
+OutputThreadInfo();
+Stopwatch timer = Stopwatch.StartNew();
+SectionTitle("Running methods synchronously on one thread.");
+MethodA();
+WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elapsed A.");
+MethodB();
+WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elapsed B.");
+MethodC();
+
+WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elapsed.");
