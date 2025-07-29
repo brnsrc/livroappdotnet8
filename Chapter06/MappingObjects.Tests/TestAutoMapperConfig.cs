c9af5346b3ff1aebe23e92ce470034fe8f4e3493
Bruno Souza <brunosrc@hotmail.com>
2025-07-29 01:49:11 -0300

    Making assertions about strings

--- Chapter06/MappingObjects.Tests/TestAutoMapperConfig.cs
+++ Chapter06/MappingObjects.Tests/TestAutoMapperConfig.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper; //To use MapperConfiguration.
-using MappingObjects.Mappers; //To use CartToSummaryMapper.
+using MappingObjects.Mappers;
+using Northwind.EntityModels;
+using Northwind.ViewModels; //To use CartToSummaryMapper.
 namespace MappingObjects.Tests;
 
 public class TestAutoMapperConfig
@@ -10,5 +12,27 @@ public class TestAutoMapperConfig
         MapperConfiguration config = CartToSummaryMapper.
             GetMapperConfiguration();
         config.AssertConfigurationIsValid();
+    }
+
+    [Fact]
+    public void TestSummaryContent()
+    {
+        //Arrange
+        var customer = new Customer("Bruno", "Souza");
+        var listItem = new List<LineItem>
+        {
+            new(ProductName: "Computer", UnitPrice: 300.5M, Quantity: 5),
+            new(ProductName: "TV", UnitPrice: 150.5M, Quantity: 10)
+        };
+
+        //Act
+        var cart = new Cart(customer, listItem);
+        var config = CartToSummaryMapper.GetMapperConfiguration();
+        IMapper mapper = config.CreateMapper();
+        Summary summary = mapper.Map<Cart, Summary>(cart);
+
+        //Assert
+        Assert.Equal("Bruno Souza", summary.FullName);
+        Assert.Equal(3007.5M, summary.Total);
     }
 }
