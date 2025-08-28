44c9f78ce64ac2254ea049f5f55ab1b486361ced
Bruno Souza <brunosrc@hotmail.com>
2025-08-28 19:23:32 -0300

    Exploring Node Time in a console app - Step 5

--- Chapter07/WorkingWithNodaTime/Program.cs
+++ Chapter07/WorkingWithNodaTime/Program.cs
@@ -10,3 +10,24 @@ WriteLine($"Now (DateTimeZone): {nowInUTc.Zone}");
 WriteLine($"Now (ZoneDateTime): {nowInUTc}");
 WriteLine($"Now (DST): {nowInUTc.IsDaylightSavingTime()}");
 WriteLine();
+
+// Use the tzdb provider to get the time zone for US Pacific.
+// To use .NET compatible time zones, use the Bcl provider.
+DateTimeZone zonePT = DateTimeZoneProviders.Tzdb["US/Pacific"];
+ZonedDateTime nowInPT = now.InZone(zonePT);
+WriteLine($"Now (DateTimeZone): {nowInPT.Zone}");
+WriteLine($"Now (ZoneDteTime): {nowInPT}");
+WriteLine($"Now (DST): {nowInPT.IsDaylightSavingTime()}");
+WriteLine();
+
+DateTimeZone zoneUK = DateTimeZoneProviders.Tzdb["Europe/London"];
+ZonedDateTime nowInUK = now.InZone(zoneUK);
+WriteLine($"Now (DateTimeZone): {nowInUK.Zone}");
+WriteLine($"Now (ZonedDateTime): {nowInUK}");
+WriteLine($"now (DST): {nowInUK.IsDaylightSavingTime()}");
+WriteLine();
+LocalDateTime nowInLocal = nowInUTc.LocalDateTime;
+WriteLine($"Now (LocalDateTime): {nowInLocal}");
+WriteLine($"Now (LocalDate): {nowInLocal.Date}");
+WriteLine($"Now (LocalTime): {nowInLocal.TimeOfDay}");
+
